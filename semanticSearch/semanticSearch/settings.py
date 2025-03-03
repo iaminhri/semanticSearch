654e1d7f43f18887d27d6aa9fec496f621ad170c
iaminhri <iamin.hri@gmail.com>
2025-03-02 21:23:47 -0500

    debug true

--- semanticSearch/semanticSearch/settings.py
+++ semanticSearch/semanticSearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(int(os.environ.get('DEBUG', 0))) # 1 == True
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
